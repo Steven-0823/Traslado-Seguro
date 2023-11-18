554addbd099a7dc973f065d7a8f8e3033b8d27fe
joan.henao <henaobermeosteven@gmail.com>
2023-11-18 14:27:14 -0500

    Creacion de Index Cliente

---
+++ Pages/Clientes/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Traslado_Seguro.Data;
+using Traslado_Seguro.Models;
+
+namespace Traslado_Seguro.Pages.Clientes
+{
+    public class IndexModel : PageModel
+    {
+		private readonly trasladoSeguroContext _context;
+
+		public IndexModel(trasladoSeguroContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Cliente> Clientes { get; set; } = default!;
+		public async Task OnGetAsync()
+		{
+			if (_context.Clientes != null)
+			{
+				Clientes = await _context.Clientes.ToListAsync();
+
+			}
+		}
+	}
+}
