9f714ab5dea9a9f574a52a4357e217a41e380d2c
joan.henao <henaobermeosteven@gmail.com>
2023-11-19 14:04:12 -0500

    Configuracion Final de Autenticacion mas la cracion de Logout

--- Pages/Servicio_Transporte/Index.cshtml.cs
+++ Pages/Servicio_Transporte/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using Traslado_Seguro.Models;
 
 namespace Traslado_Seguro.Pages.Servicio_Transporte
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly trasladoSeguroContext _context;
